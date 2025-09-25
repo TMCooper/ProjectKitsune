c038f7ae60b54a926fa260990c265faf9455607a
TMCooper <pokemon160377@gmail.com>
2025-09-25 21:32:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+import os, json
 from src.api import Yui
 from src.backend import Cardinal
 
+PATH = os.getcwd()
+PATH_ID = PATH + "\AllAnimeId.json"
+
+
 def main():
-    Yui.app.run(debug=True)
-    
-    # Cardinal.requestsAllid()
+    if os.path.exists(PATH_ID): # Vérification si le fichier AllAnimeId.json est disponible si oui alors il lance l'api sinon il crée et remplit le fichier
+        Yui.app.run(debug=True)
+
+    else:
+        print("Fichier non trouvée... créeation...")
+        Cardinal.requestsAllid()
+        main()
 
 if __name__ == "__main__":
     main()
